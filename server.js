cf9e414d98d71bc8e5cc5640ec2508fa13178387
AX DEV INVESTMENTS <ax.dev.investments@gmail.com>
2026-09-02 15:06:42 -0400

    Set default PORT to 3001

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const {
 } = require('@whiskeysockets/baileys');
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
